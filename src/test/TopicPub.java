9dd0add0d8ea2b20a4516ce6bed7eb6242ae89b0
wenthywang <946374340@qq.com>
2018-03-12 09:55:02 +0800

    添加消息发布者和订阅者测试类

---
+++ src/test/TopicPub.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright(c) Guangzhou JiaxinCloud Science & Technology Ltd. 
+ */
+package test;
+
+import org.redisson.api.RCountDownLatch;
+import org.redisson.api.RTopic;
+import org.redisson.api.RedissonClient;
+
+/**
+ * <pre>
+ * redisson client demo。
+ * </pre>
+ * @author 王文辉  wangwenhui@jiaxincloud.com
+ * @version 1.00.00
+ * @date 2018年3月9日 
+ * <pre>
+ * 修改记录
+ *    修改后版本:     修改人：  修改日期:     修改内容: 
+ * </pre>
+ */
+public class TopicPub {
+	
+	
+	 public static String ip = "redis://111.230.24.31";  
+	 public static   String port = "6379";  
+	 public static    String password="test";
+  
+	 
+	 
+    public static void main(String[] args) {
+    	testPub();
+	}  
+    
+  /**
+   * 消息发布者
+   */
+    public static  void testPub() {  
+    	RedissonClient	 client = RedisUtils.getInstance().getRedisson(ip, port,password);  
+    	RTopic<String> rTopic=RedisUtils.getInstance().getRTopic(client, "testTopic");  
+      System.out.println(rTopic.publish("今天是儿童节，大家儿童节快乐3,wocao"));  
+      //发送完消息后 让订阅者不再等待  
+      RCountDownLatch rCountDownLatch=RedisUtils.getInstance().getRCountDownLatch(client, "testCountDownLatch");  
+      rCountDownLatch.countDown();  
+        RedisUtils.getInstance().closeRedisson(client);  
+    }  
+
+}
---
+++ src/test/TopicSub.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright(c) Guangzhou JiaxinCloud Science & Technology Ltd. 
+ */
+package test;
+
+import org.redisson.api.RCountDownLatch;
+import org.redisson.api.RTopic;
+import org.redisson.api.RedissonClient;
+import org.redisson.api.listener.MessageListener;
+
+/**
+ * <pre>
+ * redisson client demo。
+ * </pre>
+ * @author 王文辉  wangwenhui@jiaxincloud.com
+ * @version 1.00.00
+ * @date 2018年3月9日 
+ * <pre>
+ * 修改记录
+ *    修改后版本:     修改人：  修改日期:     修改内容: 
+ * </pre>
+ */
+public class TopicSub {
+	
+	
+	 public static String ip = "redis://111.230.24.31";  
+	 public static   String port = "6379";  
+	 public static    String password="test";
+  
+	 
+	 
+    public static void main(String[] args) {
+    	testSub();
+	}   
+    
+    /**
+     * 订阅者
+     */
+    public static  void testSub() {  
+    	RedissonClient	 client = RedisUtils.getInstance().getRedisson(ip, port,password);  
+      RTopic<String> rTopic=RedisUtils.getInstance().getRTopic(client, "testTopic");  
+      rTopic.addListener(new MessageListener<String>() {  
+
+			@Override
+			public void onMessage(String channel, String msg) {
+			    // TODO Auto-generated method stub  
+              System.out.println("你发布的是:"+msg);  
+				
+			}  
+      });  
+      //等待发布者发布消息  
+      RCountDownLatch rCountDownLatch=RedisUtils.getInstance().getRCountDownLatch(client, "testCountDownLatch");  
+      rCountDownLatch.trySetCount(1);  
+      try {
+		rCountDownLatch.await();
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}  
+      //链接不需要关闭
+        //需要实时订阅发布者消息
+//        RedisUtils.getInstance().closeRedisson(client);  
+    }  
+  
+//      
+
+}
